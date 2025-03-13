63ad9e1308051d27b10c12b21b1ca8f3677a49d1
MigdalysFlores <migdalysjfloresr@gmail.com>
2025-03-13 16:28:53 -0300

    Update Ejemplo_para_github.java
    Actualizacion de clase de ejemplo para github

--- src/curso_selenium/Ejemplo_para_github.java
+++ src/curso_selenium/Ejemplo_para_github.java
@@ -4,7 +4,7 @@ public class Ejemplo_para_github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        // Esta es una modificacion al archivo de ejemplo github
 	}
 
 }
